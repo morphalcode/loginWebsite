33ad2a2ae27f759ec4186591093325569faab259
sfcompsci <stefanfourie@outlook.com>
2023-02-02 12:19:21 +0400

    Create Python app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return "<p>homepage!</p>"
+
+@app.route("/first")
+def return_first():
+    return render_template("first.html")
+
+@app.route("/second")
+def return_second():
+    return render_template("second.html")
